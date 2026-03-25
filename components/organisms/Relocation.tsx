08fbd165f9ee2e33f18b42b9ec15acd3ef83f069
Guillermo Rodas <guillermo.rodas@usekojo.com>
2026-03-26 00:36:26 +0100

    chore: commit changes

--- components/organisms/Relocation.tsx
+++ components/organisms/Relocation.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Country from "../molecules/Country";
-import Icon from "../atoms/Icon";
 
 const Relocation: React.FC = () => {
   return (
@@ -8,7 +7,22 @@ const Relocation: React.FC = () => {
       <div className="relocation">
         <Country flag="colombia" description="From Colombia" />
         <div className="connector-icon">
-          <Icon size="lg" name="airplane" />
+          <svg
+            className="connector-airplane"
+            width={30}
+            height={30}
+            viewBox="0 0 24 24"
+            aria-hidden
+          >
+            <path
+              className="connector-airplane-path"
+              d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"
+              fill="var(--relocation-connector-icon)"
+              stroke="var(--relocation-connector-stroke)"
+              strokeWidth={3}
+              strokeLinejoin="round"
+            />
+          </svg>
         </div>
         <div className="divider" />
         <Country flag="sweden" description="Living in Sweden" />
@@ -26,6 +40,16 @@ const Relocation: React.FC = () => {
         .connector-icon {
           position: relative;
           top: -20px;
+        }
+
+        .connector-airplane {
+          display: block;
+          width: 30px;
+          height: 30px;
+        }
+
+        .connector-airplane-path {
+          paint-order: stroke fill;
         }
 
         .divider {
